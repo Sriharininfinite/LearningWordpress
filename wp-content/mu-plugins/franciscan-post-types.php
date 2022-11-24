343cdbfa260e0ff12b6d14e84b65627b5077915b
Srihari Nelapati <sriharin@infinite.com>
2022-11-24 12:43:55 +0530

    addevents

--- wp-content/mu-plugins/franciscan-post-types.php
+++ wp-content/mu-plugins/franciscan-post-types.php
@@ -2,6 +2,10 @@
 
 function franciscan_post_types() {
   register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
     'public' => true,
     'show_in_rest' => true,
     'labels' => array(
